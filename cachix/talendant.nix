282bf4eb69c02e7721468955b367e2c7082b3396
Eric B.Merritt <eric@merritt.tech>
2020-04-03 08:28:53 -0700

    Nix home maneger implementation

---
+++ cachix/talendant.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://talendant.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "talendant.cachix.org-1:0I5zNdVSmc5mJV9LaQ0J74JLgWWH4pjavXkd6LXFwyA="
+    ];
+  };
+}
+    
--- configuration.nix
+++ configuration.nix
@@ -2,27 +2,20 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, options,... }:
-let
-  all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {};
-  talpkgs = (builtins.fetchGit {
-    url = "ssh://git@gitlab.com/talendant/talpkgs.git";
-    ref = "v0.0.0-build.10.c82edc1";
-  });
-in {  
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./cachix.nix
-    ];
+{ config, pkgs, options, ... }:
+ {
+  imports = [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./cachix.nix
+    <home-manager/nixos>
+  ];
 
-  boot.initrd.luks.devices = 
-   {
-      root = {
-         device = "/dev/nvme0n1p2";
-         preLVM = true;
-      };
-   };
+  boot.initrd.luks.devices = {
+    root = {
+      device = "/dev/nvme0n1p2";
+      preLVM = true;
+    };
+  };
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -45,55 +38,30 @@ in {
 
   # Select internationalisation properties.
   i18n = {
-     consoleFont = "Lat2-Terminus16";
-     consoleKeyMap = "dvorak";
-     defaultLocale = "en_US.UTF-8";
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "dvorak";
+    defaultLocale = "en_US.UTF-8";
   };
   nixpkgs.config.allowUnfree = true;
- 
-  virtualisation.docker.enable = true;   
- 
+
+  virtualisation.docker.enable = true;
+
   # Set your time zone.
   time.timeZone = "US/Pacific";
-
-  nixpkgs.overlays = (import talpkgs);
-
-  nix.nixPath =
-    # Prepend default nixPath values.
-    options.nix.nixPath.default ++ 
-    # Append our nixpkgs-overlays.
-    [ "nixpkgs-overlays=${talpkgs}/default.nix" ]
-  ;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-     firefox
-     git
-     networkmanagerapplet
-     wget 
-     curl
-     vim
-     which
-     gnupg
-     keybase
-     keybase-gui
-     zsh
-     zsh-prezto
-     emacs
-     vscode
-     zip
-     unzip
-     wget
-     gnumake
-     killall
-     google-chrome
-     cabal2nix
-     nix-prefetch-git
-     cabal-install
-     haskellPackages.hpack
-     (all-hies.selection { selector = p: { inherit (p) ghc865 ghc864; }; })
- ];
+    networkmanagerapplet
+    neovim
+    which
+    gnupg
+    keybase
+    keybase-gui
+    zsh
+    zsh-prezto
+    vscode
+  ];
 
   # enable keybase
   services.keybase.enable = true;
@@ -102,9 +70,14 @@ in {
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
+
+  services.printing.enable = true;
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
@@ -122,44 +95,69 @@ in {
   sound.enable = true;
   hardware.pulseaudio.enable = true;
   hardware.pulseaudio.package = pkgs.pulseaudioFull;
-  hardware.pulseaudio.extraModules = [ pkgs.pulseaudio-modules-bt ]; 
+  hardware.pulseaudio.extraModules = [ pkgs.pulseaudio-modules-bt ];
 
-  hardware.bluetooth.extraConfig = "
-    [General]
-    Enable=Source,Sink,Media,Socket
-  ";
-  
+  hardware.bluetooth.extraConfig =
+    "\n    [General]\n    Enable=Source,Sink,Media,Socket\n  ";
+
   # Enable the X11 windowing system.
   services.xserver = {
     enable = true;
     layout = "us";
     xkbVariant = "dvorak";
-    xkbOptions = "ctrl:swapcaps";
+    xkbOptions = "ctrl:swapescape";
   };
 
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = false;
   services.xserver.desktopManager.gnome3.enable = true;
- 
+
   services.dbus.packages = with pkgs; [ gnome3.dconf gnome2.GConf ];
 
   services.tlp.enable = true;
 
   users.users.eric = {
-     isNormalUser = true;
-     createHome = true;
-     shell = pkgs.zsh;
-     extraGroups = [ "wheel" "video" "audio" "disk" "networkmanager" "docker" ];
+    isNormalUser = true;
+    createHome = true;
+    shell = pkgs.zsh;
+    extraGroups = [ "wheel" "video" "audio" "disk" "networkmanager" "docker" ];
   };
 
-  programs.zsh.enable = true;
-  programs.zsh.interactiveShellInit = ''
-    source ${pkgs.zsh-prezto}/init.zsh
+  home-manager.users.eric = { pkgs, ... }: {
+    home.packages = [ pkgs.emacs 
+                      pkgs.neovim 
+		      pkgs.firefox
+		      pkgs.google-chrome
+		    ];
+    programs.zsh.enable = true;
 
-    # Custom Git Commands
-    git config --global user.email "eric@merritt.tech" 2> /dev/null
-    git config --global user.name "Eric B Merritt" 2> /dev/null
-  '';
+    programs.command-not-found.enable = true;
+
+    programs.git = {
+      package = pkgs.gitAndTools.gitFull;
+      enable = true;
+      userName = "Eric B. Merritt";
+      userEmail = "eric@merritt.tech";
+    };
+
+    programs.fzf = {
+      enable = true;
+      enableBashIntegration = true;
+      defaultCommand = "${pkgs.ripgrep}/bin/rg --files";
+    };
+
+    home.file = {
+      ".tmux.conf" = {
+        text = ''
+          set -g mouse on
+          set -g focus-events on
+        '';
+      };
+
+    };
+
+  };
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
